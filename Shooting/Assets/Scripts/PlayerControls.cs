b805cfab4e5d1a7711345295a0f28eff34089f93
HominMoon <74246315+HominMoon@users.noreply.github.com>
2022-08-12 20:04:46 +0900

    Day5
    add scripts

---
+++ Shooting/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControls : MonoBehaviour
+{
+    [SerializeField] float controllSpeed = 10f;
+    [SerializeField] float xRange = 5f;
+    [SerializeField] float yRange = 3.5f;
+
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float controllPitchFactor = -10f;
+    [SerializeField] float positionYawFactor = 3f;
+    [SerializeField] float controlRollFactor = -20f;
+
+    float xVal;
+    float yVal;
+
+    // Update is called once per frame
+    void Update()
+    {   ProcessRotation();
+        ProcessTranslation();
+        
+    }
+
+    void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlValue = yVal * controllPitchFactor;
+
+        float pitch = pitchDueToPosition * pitchDueToControlValue;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xVal * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        xVal = Input.GetAxis("Horizontal");
+        yVal = Input.GetAxis("Vertical");
+
+        float xOffset = xVal * Time.deltaTime * controllSpeed;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        float yOffset = yVal * Time.deltaTime * controllSpeed;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3
+        (clampXPos, clampYPos, transform.localPosition.z);
+    }
+
+
+
+
+    // float horizontalThrow = movement.ReadValue<Vector2>().x;
+    // float verticalThrow = movement.ReadValue<Vector2>().y;
+
+    //[SerializeField] InputAction movement;
+
+    // private void OnEnable() {
+    //     movement.Enable();
+    // }
+    // private void OnDisable() {
+    //     movement.Disable();
+    // }
+}
